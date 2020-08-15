8b751498f910a6dde249f4da11af50325154f435
nickbreaton <nick@breaton.com>
2020-08-14 22:24:55 -0400

    Batch writes

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -11,6 +11,8 @@ type ResponsePayload = {
 
 type RequestPayload = Partial<ResponsePayload>
 
+const BATCH_TIME = Math.floor(1000 / 60);
+
 export class ModernFormsHttpClient {
   constructor(private readonly ip: string) {}
 
@@ -18,12 +20,37 @@ export class ModernFormsHttpClient {
     () => axios
       .post<ResponsePayload>(`http://${this.ip}/mf`, { queryDynamicShadowData: 1 })
       .then(res => res.data),
-    { maxAge: 10 },
+    { maxAge: BATCH_TIME },
   )
 
+  nextUpdatePayloads: RequestPayload[] = []
+  nextUpdateCallbacks: Array<(promise: Promise<ResponsePayload>) => void> = []
+
   update = (payload: RequestPayload) => {
-    return axios
-      .post<ResponsePayload>(`http://${this.ip}/mf`, payload)
-      .then(res => res.data);
+    if (this.nextUpdatePayloads.length === 0) {
+      setTimeout(() => {
+        const payload = this.nextUpdatePayloads.reduce<RequestPayload>((final, current) => {
+          return { ...final, ...current };
+        }, {});
+
+        const callbacks = this.nextUpdateCallbacks;
+
+        this.nextUpdatePayloads = [];
+        this.nextUpdateCallbacks = [];
+
+        const promise = axios
+          .post<ResponsePayload>(`http://${this.ip}/mf`, payload)
+          .then(res => res.data);
+
+        callbacks.forEach(callback => callback(promise));
+      }, BATCH_TIME);
+    }
+
+    return new Promise((res, rej) => {
+      this.nextUpdatePayloads.push(payload);
+      this.nextUpdateCallbacks.push((promise) => {
+        promise.then(res, rej);
+      });
+    });
   }
 }
